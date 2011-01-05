45332147680d5c46df694a14af609b8bc7a81894
Bryan Ray <bryan@bryanray.net>
2011-01-05 14:16:30 -0600

    Generating the new skeleton stuff for the drops controller.

---
+++ app/controllers/drops_controller.rb
@@ -0,0 +1,23 @@
+class DropsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def delete
+  end
+
+end
